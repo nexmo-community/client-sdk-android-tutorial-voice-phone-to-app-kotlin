056ed09cfb8e0f7360c0cba54aa9572897ac4d44
Igor <igor.wojda@gmail.com>
2020-09-07 15:26:32 +0200

    clean up

--- app/src/main/java/com/vonage/phonetoapp/MainActivity.kt
+++ app/src/main/java/com/vonage/phonetoapp/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         // login client
-        client.login("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1OTk0ODQ3NzEsImp0aSI6ImMzOTAwYTgwLWYxMGMtMTFlYS1iNTM2LWFkNjZjOTE3YzgzYyIsImV4cCI6MTU5OTUwNjM3MCwiYWNsIjp7InBhdGhzIjp7Ii8qL3VzZXJzLyoqIjp7fSwiLyovY29udmVyc2F0aW9ucy8qKiI6e30sIi8qL3Nlc3Npb25zLyoqIjp7fSwiLyovZGV2aWNlcy8qKiI6e30sIi8qL2ltYWdlLyoqIjp7fSwiLyovbWVkaWEvKioiOnt9LCIvKi9hcHBsaWNhdGlvbnMvKioiOnt9LCIvKi9wdXNoLyoqIjp7fSwiLyova25vY2tpbmcvKioiOnt9fX0sInN1YiI6IkFsaWNlIiwiYXBwbGljYXRpb25faWQiOiJjNWJmNmNmZi02N2ExLTQ4ZmQtYjM3Yi0zZWM5N2Q0MjY4NjkifQ.Nb5TfU5MoLcZdAeBqpUAWVqUejZm_0dJpnIqqh-YVr1CchJPmWfulUAiWtC-ze7Tbx9-kzl1keOx6ONq05fL6nfC_J7UlXPrV59UHIlQiuu6kA1yl3mjDjDGUevIKObFyz2dF-m11JG1RqsecpGyKV85oKg0t0gMTGPqeGxXoam1wwhBfLzxy4VLwhudG7H5F_HipS9BF1wm185xk556ewXF0fUW7Y2N5xBU3z5TU9W8vecI8pQk86w8x-1BXA3jItlTfJr-P5iOabZFb4DEjwvHBW87GE_ygYebjal6H-DchGPDF9w9QZS79LhFPreHy34mCMGSEjy7WD7CSvXvMg")
+        client.login("ALICE_TOKEN")
 
         // listen for incoming calls
         client.addIncomingCallListener { it ->
