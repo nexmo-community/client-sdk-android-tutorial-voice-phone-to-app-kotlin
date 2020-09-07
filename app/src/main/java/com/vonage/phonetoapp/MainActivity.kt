967554d59c6567b556aff09b6a04771bdde570cd
Igor <igor.wojda@gmail.com>
2020-09-07 17:42:56 +0200

    Add permission request

--- app/src/main/java/com/vonage/phonetoapp/MainActivity.kt
+++ app/src/main/java/com/vonage/phonetoapp/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.vonage.phonetoapp
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.nexmo.client.NexmoCall
 import com.nexmo.client.NexmoClient
 import com.nexmo.client.request_listener.NexmoApiError
@@ -19,6 +23,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Request permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), 123);
+        }
 
         // Init Nexmo client
         val client: NexmoClient = NexmoClient.Builder().build(this)
